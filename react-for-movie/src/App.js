6c4c36dd958342cdd7961b0ac6d4b3466dff285c
Hyunji14 <blackit14@gmail.com>
2023-10-16 20:53:54 +0900

    useEffect practice

--- react-for-movie/src/App.js
+++ react-for-movie/src/App.js
@@ -1,11 +1,43 @@
 import Button from './Button';
 import styles from './css/App.module.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log('i run all the time');
+
+  useEffect(() => {
+    console.log('I run only Onced');
+  }, []);
+
+  // 키워드가 변할때만 실행
+  useEffect(() => {
+    if (keyword !== '' && keyword.length >= 5) {
+      console.log("I run when 'keyword' changes", keyword);
+    }
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("I run when 'counter' changes", counter);
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("I run when 'keyword & counter' changes", counter);
+  }, [keyword, counter]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back</h1>
-      <Button text={'Continue'} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type='text'
+        placeholder='Search here...'
+      />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
